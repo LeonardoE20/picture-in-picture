8e0c5052e7a1d9936900d27954fbc77595037fab
Leonardo Escudero <leonardolearns@gmail.com>
2020-07-26 14:31:36 -0700

    added comments so that others will understand what is going on within the code like the  async function I created

--- script.js
+++ script.js
@@ -4,8 +4,11 @@ const button = document.getElementById('button');
 // Prompt to select media stream, pass to video element, then play
 async function selectMediaStream() {
   try {
+    // setting const that will have media stream data, waiting to assign until user selects which screen or window they want to share
     const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    // passing that mediastream into video object as its source object.
     videoElement.srcObject = mediaStream;
+    // when video loads its metadata it will call a function that will call and play a video
     videoElement.onloadedmetadata = () => {
       videoElement.play();
     }
