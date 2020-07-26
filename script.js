04c1a00897d606240e9ca57cb482e1c024cee38d
Leonardo Escudero <leonardolearns@gmail.com>
2020-07-26 15:43:50 -0700

    added more comments to understand whatis going on thoroughly in the project

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const button = document.getElementById('button');
 // Prompt to select media stream, pass to video element, then play
 async function selectMediaStream() {
   try {
-    // setting const that will have media stream data, waiting to assign until user selects which screen or window they want to share
+    // setting const that will have media stream data, waiting to assign until user selects which screen or window they want to share. navigator.mediaDevices is a web API
     const mediaStream = await navigator.mediaDevices.getDisplayMedia();
     // passing that mediastream into video object as its source object.
     videoElement.srcObject = mediaStream;
@@ -16,13 +16,13 @@ async function selectMediaStream() {
     // Catch Error Here
   }
 }
-
+// using and triggering the button to start the picture in picture api functionality instead of using it in the video element (video player) in the options menu
 button.addEventListener('click', async () => {
-  // Disable Button
+  // Disable Button when we click on it
   button.disabled = true;
-  // Start Picture in Picture
+  // Start Picture in Picture by waiting the video element to request the api and not do anything else until that happens
   await videoElement.requestPictureInPicture();
-  // Reset Button
+  // Reset Button, will happen if we successfully request our picture in picture; if not our button will remain disabled
   button.disabled = false;
 });
 
